993ae60e27c70bc38370c1c98bff01ace58ebc63
chenglingr <286863096@qq.com>
2016-05-12 14:21:13 +0800

    珊

--- Web/WEB/ashx/delUser.ashx.cs
+++ Web/WEB/ashx/delUser.ashx.cs
@@ -14,7 +14,20 @@ namespace Web.WEB.ashx
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "text/plain";
-            context.Response.Write("Hello World");
+            string json = "{}";
+            string action = context.Request.Form["Action"];
+            if (action == "Del")//删除操作
+            {
+                string DelNumS = context.Request.Form["DelNumS"];//获取批量删除的编号
+                BBS.BLL.BBSUsers bll = new BBS.BLL.BBSUsers();
+                if (bll.DeleteList(DelNumS))
+                {
+                    json = "{'info':'删除成功'}";
+                }
+                else
+                { json = "{'info':'删除失败'}"; }
+            }
+            context.Response.Write(json);
         }
 
         public bool IsReusable
