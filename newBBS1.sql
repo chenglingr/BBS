99df0969526a7f58dd217b1994fa9361ba3ff3a1
chenglingr <286863096@qq.com>
2016-05-12 21:30:30 +0800

    ok

--- newBBS1.sql
+++ newBBS1.sql
@@ -14,6 +14,15 @@ CREATE TABLE [BBSReply] (
 ALTER TABLE [BBSReply] WITH NOCHECK ADD  CONSTRAINT [PK_BBSReply] PRIMARY KEY  NONCLUSTERED ( [RID] )
 SET IDENTITY_INSERT [BBSReply] ON
 
+INSERT [BBSReply] ([RID],[RTID],[RSID],[RUID],[RTopic],[RContents],[RTime],[RClickCount]) VALUES ( 2,1,1,1,N'test1',N'432432',N'2016/5/8 14:46:36',0)
+INSERT [BBSReply] ([RID],[RTID],[RSID],[RUID],[RTopic],[RContents],[RTime],[RClickCount]) VALUES ( 3,1,1,1,N'test2',N'人人公认',N'2016/5/8 14:47:20',0)
+INSERT [BBSReply] ([RID],[RTID],[RSID],[RUID],[RTopic],[RContents],[RTime],[RClickCount]) VALUES ( 4,1,1,1,N'test4',N'4让软',N'2016/5/8 14:47:30',0)
+INSERT [BBSReply] ([RID],[RTID],[RSID],[RUID],[RTopic],[RContents],[RTime],[RClickCount]) VALUES ( 5,1,1,1,N'test5',N'54354',N'2016/5/8 15:25:09',0)
+INSERT [BBSReply] ([RID],[RTID],[RSID],[RUID],[RTopic],[RContents],[RTime],[RClickCount]) VALUES ( 6,1,1,1,N'test4',N'54354',N'2016/5/8 15:25:20',0)
+INSERT [BBSReply] ([RID],[RTID],[RSID],[RUID],[RTopic],[RContents],[RTime],[RClickCount]) VALUES ( 7,1,1,1,N'特色通过',N'54353334',N'2016/5/8 15:25:25',0)
+INSERT [BBSReply] ([RID],[RTID],[RSID],[RUID],[RTopic],[RContents],[RTime],[RClickCount]) VALUES ( 8,1,1,1,N'无标题444',N'44444444444444444444444444444
+0-0',N'2016/5/8 15:36:40',0)
+INSERT [BBSReply] ([RID],[RTID],[RSID],[RUID],[RTopic],[RContents],[RTime],[RClickCount]) VALUES ( 9,1,1,1,N'无标题',N'炒作宣传下',N'2016/5/8 15:40:37',0)
 
 SET IDENTITY_INSERT [BBSReply] OFF
 if exists (select * from sysobjects where id = OBJECT_ID('[BBSSection]') and OBJECTPROPERTY(id, 'IsUserTable') = 1) 
@@ -36,6 +45,7 @@ INSERT [BBSSection] ([SID],[SName],[SMasterID],[SStatement],[SClickCount],[STopi
 INSERT [BBSSection] ([SID],[SName],[SMasterID],[SStatement],[SClickCount],[STopicCount]) VALUES ( 5,N'时尚',1,N'时尚“时尚”这个词现在已是很流行的了，英文为fashion，该词来源于拉丁文“facio or factio”，意思是“making or doing”（制造的或人为的），是对一种外表行为模式的崇尚方式。追求时尚似已蔚然成风，时尚就是在特定时段内率先由少数人实验、而后来为社会大众所崇尚和仿效的生活样式。',0,0)
 INSERT [BBSSection] ([SID],[SName],[SMasterID],[SStatement],[SClickCount],[STopicCount]) VALUES ( 7,N'娱乐',1,N'观众参与的娱乐，可以是被动的如看歌剧，或者主动的如玩电脑游戏。而提供娱乐的行业称为娱乐行业。消闲、游戏、阅读还有视觉艺术欣赏普遍地不被认为是娱乐。因为娱乐的一般定义是需要有观看者可见的表演提供者。',0,0)
 INSERT [BBSSection] ([SID],[SName],[SMasterID],[SStatement],[SClickCount],[STopicCount]) VALUES ( 8,N'情感',1,N'情感是态度这一整体中的一部分，它与态度中的内向感受、意向具有协调一致性，是态度在生理上一种较复杂而又稳定的生理评价和体验。情感包括道德感和价值感两个方面，具体表现为爱情、幸福、仇恨、厌恶、美感等等。',0,0)
+INSERT [BBSSection] ([SID],[SName],[SMasterID],[SStatement],[SClickCount],[STopicCount]) VALUES ( 9,N'其他',1,N'随便说很么',0,0)
 
 SET IDENTITY_INSERT [BBSSection] OFF
 if exists (select * from sysobjects where id = OBJECT_ID('[BBSTopic]') and OBJECTPROPERTY(id, 'IsUserTable') = 1) 
@@ -46,7 +56,7 @@ CREATE TABLE [BBSTopic] (
 [tsid] [int]  NOT NULL,
 [tuid] [int]  NOT NULL,
 [treplycount] [int]  NULL DEFAULT (0),
-[TTopic] [varchar]  (30) NOT NULL,
+[TTopic] [varchar]  (60) NOT NULL,
 [TContents] [text]  NOT NULL,
 [TTime] [datetime]  NULL DEFAULT (getdate()),
 [TClickCount] [int]  NULL DEFAULT (0),
@@ -55,11 +65,13 @@ CREATE TABLE [BBSTopic] (
 ALTER TABLE [BBSTopic] WITH NOCHECK ADD  CONSTRAINT [PK_BBSTopic] PRIMARY KEY  NONCLUSTERED ( [tid] )
 SET IDENTITY_INSERT [BBSTopic] ON
 
-INSERT [BBSTopic] ([tid],[tsid],[tuid],[treplycount],[TTopic],[TContents],[TTime],[TClickCount],[TLastClickT]) VALUES ( 1,1,1,0,N'我是魔王',N'DEVIL MAY CRY',N'2011/1/10 0:00:00',5,N'2011/6/24 0:00:00')
-INSERT [BBSTopic] ([tid],[tsid],[tuid],[treplycount],[TTopic],[TContents],[TTime],[TClickCount],[TLastClickT]) VALUES ( 2,1,2,0,N'灌水来着~~',N'DEVIL MAY CRY',N'2011/4/10 0:00:00',4,N'2011/6/28 0:00:00')
-INSERT [BBSTopic] ([tid],[tsid],[tuid],[treplycount],[TTopic],[TContents],[TTime],[TClickCount],[TLastClickT]) VALUES ( 3,2,1,0,N'灌水来着1~~',N'DEVIL MAY CRY',N'2011/1/10 0:00:00',3,N'2011/6/27 0:00:00')
+INSERT [BBSTopic] ([tid],[tsid],[tuid],[treplycount],[TTopic],[TContents],[TTime],[TClickCount],[TLastClickT]) VALUES ( 1,1,1,0,N'南海舰队演练夺岛备受关注 专家：开战首当其冲',N'人民网北京5月3日电 （记者 黄子娟）近日，南海舰队组织了两场联合演练备受关注。一场是某登陆舰支队在南海海域组织的夺岛演练，另一场是某潜艇支队在某海域与水面舰艇航空兵展开协同演练。军事专家尹卓在接受央视《今日关注》采访时表示，岛礁争夺作战，是南海舰队每年的必练科目。这种演练是常态化的例行演习，但每年都会补充新的内容，增加新的科目。据央视报道，南海舰队某登陆舰支队在南海组织的夺岛演练中，中国海军现役最强的两栖登陆舰和气垫船等装备悉数亮相，演练模拟夺取千里之外的某座小岛，演习采用红蓝对抗的模式。红方登陆舰艇编队，由两栖船坞登陆舰“长白山”舰担任指挥舰，蓝军则依托有利地形和强大火力优势，对红方登陆兵力实施阻击。“长白山”舰抵达某任务海域以后演练随即展开，各型登陆力量从“长白山”舰的坞舱中鱼贯而出。演练检验海军登陆作战部队在复杂环境下的登陆作战能力，提升了两栖船坞登陆舰、气垫艇和陆战部队之间的协同配合能力，有效拓展了气垫登陆艇的作战利用范围。',N'2011/1/10 0:00:00',5,N'2011/6/24 0:00:00')
+INSERT [BBSTopic] ([tid],[tsid],[tuid],[treplycount],[TTopic],[TContents],[TTime],[TClickCount],[TLastClickT]) VALUES ( 2,1,2,0,N'济南侦破儿童信息泄露案 黑客入侵免疫规划系统',N'4月6日，“齐鲁壹点”网站发布一篇题为《济南20万孩童信息被打包出售！每条信息价格一两毛》的文章，引起社会广泛关注。济南市公安局迅速行动，组织优势警力成立专案组，先后赴四川、广东、福建等地，全力开展案件侦查工作。',N'2011/4/10 0:00:00',4,N'2011/6/28 0:00:00')
+INSERT [BBSTopic] ([tid],[tsid],[tuid],[treplycount],[TTopic],[TContents],[TTime],[TClickCount],[TLastClickT]) VALUES ( 3,2,1,0,N'昆明一寺庙用活动板房搭建 网友调侃“最简陋”庙宇',N'2016年5月2日，云南昆明小康大道附近惊现“最简陋”寺庙，该寺庙系用活动板房搭建，门口悬挂“西京寺”牌匾，网友调侃，这可能是世上“最简陋”的寺庙了',N'2011/1/10 0:00:00',3,N'2011/6/27 0:00:00')
 INSERT [BBSTopic] ([tid],[tsid],[tuid],[treplycount],[TTopic],[TContents],[TTime],[TClickCount],[TLastClickT]) VALUES ( 4,2,5,0,N'灌水来着2~~',N'DEVIL MAY CRY',N'2011/1/10 0:00:00',0,N'2011/6/26 0:00:00')
 INSERT [BBSTopic] ([tid],[tsid],[tuid],[treplycount],[TTopic],[TContents],[TTime],[TClickCount],[TLastClickT]) VALUES ( 5,2,4,0,N'灌水来着3~~',N'DEVIL MAY CRY',N'2011/4/10 0:00:00',1,N'2011/6/24 0:00:00')
+INSERT [BBSTopic] ([tid],[tsid],[tuid],[treplycount],[TTopic],[TContents],[TTime],[TClickCount],[TLastClickT]) VALUES ( 7,1,1,0,N'test',N'<p>这里我</p><p>可以<span style="color:#e36c09">写一些</span>输</p><p>入<span style="background-color: rgb(192, 0, 0);">提示</span></p>',N'2016/5/8 13:34:40',0,N'2016/5/8 13:34:40')
+INSERT [BBSTopic] ([tid],[tsid],[tuid],[treplycount],[TTopic],[TContents],[TTime],[TClickCount],[TLastClickT]) VALUES ( 8,1,1,0,N'哈哈',N'<p>这里我可以写一rey</p><p>些输入提示</p><p><img src="http://localhost:61481/Web/User/upload/2016-05-08/0ea0233d-f596-41f7-9df4-859ba89a975b.jpg" _src="http://localhost:61481/Web/User/upload/2016-05-08/0ea0233d-f596-41f7-9df4-859ba89a975b.jpg" style="width: 91px; height: 119px;"/></p>',N'2016/5/8 15:51:41',0,N'2016/5/8 15:51:41')
 
 SET IDENTITY_INSERT [BBSTopic] OFF
 if exists (select * from sysobjects where id = OBJECT_ID('[BBSUsers]') and OBJECTPROPERTY(id, 'IsUserTable') = 1) 
